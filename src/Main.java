561b2bd553fba7084e47302c068eae7af314edc9
cesarsebastiandev <cesarsebastian.dev@gmail.com>
2025-06-16 21:52:43 -0600

    My first hello world in Java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from Java!");
+        System.out.println("I'm learning Java by myself");
+        System.out.print("I'm happy\n");
+        System.out.print("I like pizza. It's really good!");
+    }
+}
