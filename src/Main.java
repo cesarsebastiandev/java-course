d589f97ae53dbf319b72c5c1421d9d1a379d8785
cesarsebastiandev <cesarsebastian.dev@gmail.com>
2025-06-16 22:17:57 -0600

    Refactor hello world in Java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        //This is my first Java program
+        /*
+        This
+        is
+        a
+        multi-line
+        comment
+        */
         System.out.println("Hello World from Java!");
+        System.out.println("My name is Cesar Sebastian");
         System.out.println("I'm learning Java by myself");
         System.out.print("I'm happy\n");
-        System.out.print("I like pizza. It's really good!");
+        System.out.print("I love to play basketball. My favorite player is Curry!");
     }
 }
